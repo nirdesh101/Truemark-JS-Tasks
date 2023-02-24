833c2a304180b3fe33ef95401370a9f45d85fe1b
nirdeshpokharel <nirdeshpokharel101gmail.com>
2023-02-24 13:57:37 +0545

    variables types

---
+++ 1st JS TASK VARIABLES/script.js
@@ -0,0 +1,16 @@
+let id = 10;
+let person = `nirdesh`;
+let history = null;
+let animal;
+let married = true;
+let sym = Symbol("foo");
+let bigNum = BigInt('23003024084329023494327097497490275095705732');
+
+
+console.log(typeof(id))
+console.log(typeof(person))
+console.log(typeof(animal))
+console.log(typeof(history))
+console.log(typeof(married))
+console.log(typeof(sym))
+console.log(typeof(bigNum))
