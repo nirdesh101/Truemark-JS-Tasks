ef15de0ae9f6b6e22313ed7a0111303bfbe0a5af
nirdeshpokharel <nirdeshpokharel101gmail.com>
2023-03-01 14:01:44 +0545

    modal task

---
+++ modal task/script.js
@@ -0,0 +1,6 @@
+function toggle(){
+    let blur = document.getElementById("blur");
+    blur.classList.toggle('active')
+    let modal = document.getElementById("modal");
+    modal.classList.toggle('active')
+}
