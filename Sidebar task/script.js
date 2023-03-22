a18363f108970376ca22d4cc2da5eb22ba7f6dda
nirdeshpokharel <nirdeshpokharel101gmail.com>
2023-03-22 10:23:17 +0545

    resolved comments

--- Sidebar task/script.js
+++ Sidebar task/script.js
@@ -1,11 +1,11 @@
 function openBar(){
-    document.getElementById("sidebar").style.width = "360px";
-    document.getElementById("main").style.marginLeft = "360px";
+    document.getElementById("sidebar").style.width = "22.5rem";
+    document.getElementById("main").style.marginLeft = "22.5rem";
     document.getElementById("btn").style.display ="none";
 }
 
 function closeBar(){
-    document.getElementById("sidebar").style.width = "0px";
-    document.getElementById("main").style.marginLeft = "0px";
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
     document.getElementById("btn").style.display ="block";
 }
